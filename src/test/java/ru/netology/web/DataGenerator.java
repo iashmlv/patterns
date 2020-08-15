7daeebd126e89123e64e85d9d019712a2d27a115
Ivan Shumilov <ivan.shumilov91@gmail.com>
2020-08-15 15:24:34 +0300

    added method in generator

--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -2,13 +2,30 @@ package ru.netology.web;
 
 import com.github.javafaker.Faker;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Random;
 
 public class DataGenerator {
-    private DataGenerator() {}
+    private DataGenerator() {
+    }
+
+    public static String getDate1(int i) {
+        LocalDate dateOrder1 = LocalDate.now().plusDays(5);
+        return dateOrder1.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String getDate2() {
+        LocalDate dateOrder2 = LocalDate.now().plusDays(7);
+        return dateOrder2.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     public static class Registration {
-        private Registration() {}
+        private Registration() {
+        }
+
+
         public static UserData generateByFaker(String locale) {
             Faker faker = new Faker(new Locale(locale));
             String[] cities = {"Абакан", "Архангельск", "Барнаул", "Владикавказ", "Горно-Алтайск", "Екатеринбург",
@@ -22,6 +39,9 @@ public class DataGenerator {
                     faker.numerify("+7##########"),
                     cities[rand.nextInt(cities.length)]
             );
+        }
+
+        private static class Date1 {
         }
     }
 }
--- src/test/java/ru/netology/web/SchedulingTest.java
+++ src/test/java/ru/netology/web/SchedulingTest.java
@@ -4,46 +4,39 @@ import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SchedulingTest {
 
-    private String date1 = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    private String date2 = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String dateOrder1 = DataGenerator.getDate1(5);
+    String dateOrder2 = DataGenerator.getDate2();
 
     @Test
     void shouldSchedule() {
         open("http://localhost:9999");
         val user = DataGenerator.Registration.generateByFaker("ru");
-        val name = user.getName();
-        val phone = user.getPhoneNumber();
-        val city = user.getCity();
-        System.out.println(city);
 
-        $("span[data-test-id='city'] input").setValue(city.substring(0,2));
-        $$("div.menu div.menu-item").find(exactText(city)).click();
+        $("span[data-test-id='city'] input").setValue(user.getCity().substring(0, 2));
+        $$("div.menu div.menu-item").find(exactText(user.getCity())).click();
 
-        $("span[data-test-id='date'] input.input__control").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("span[data-test-id='date'] input.input__control").setValue(date1);
+        $("span[data-test-id='date'] input.input__control").sendKeys((Keys.chord(Keys.CONTROL+"a")+Keys.DELETE));
+        $("span[data-test-id='date'] input.input__control").setValue(dateOrder1);
 
-        $("span[data-test-id='name'] input").setValue(name);
-        $("span[data-test-id='phone'] input").setValue(phone);
+        $("span[data-test-id='name'] input").setValue(user.getName());
+        $("span[data-test-id='phone'] input").setValue(user.getPhoneNumber());
 
         $("label[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
-        $("div.notification__content").waitUntil(text("Встреча успешно запланирована на " + date1),
+        $("div.notification__content").waitUntil(text("Встреча успешно запланирована на " + dateOrder1),
                 20000);
 
-        $("span[data-test-id='date'] input.input__control").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("span[data-test-id='date'] input.input__control").setValue(date2);
+        $("span[data-test-id='date'] input.input__control").sendKeys(Keys.chord(Keys.COMMAND+"a")+Keys.BACK_SPACE);;
+        $("span[data-test-id='date'] input.input__control").setValue(dateOrder2);
 
         $$("button").find(exactText("Запланировать")).click();
         $("div[data-test-id='replan-notification'] button").waitUntil(visible, 20000).click();
-        $("div.notification__content").waitUntil(text("Встреча успешно запланирована на " + date2),
+        $("div.notification__content").waitUntil(text("Встреча успешно запланирована на " + dateOrder2),
                 20000);
     }
 }
